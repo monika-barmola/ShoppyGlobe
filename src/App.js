f16f3ceab2917f20ab9e2cb23c4ba7cedea40cf8
Monika Barmola <barmolamonika@gmail.com>
2025-02-25 11:38:15 +0530

    add lazy laoding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import ProductList from "./components/ProductList";
-import ProductDetail from "./components/ProductDetail";
-import Cart from "./components/Cart";
 import NotFound from "./components/NotFound";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+
+const ProductList = lazy(() => import("./components/ProductList"));
+const ProductDetail = lazy(() => import("./components/ProductDetail"));
+const Cart = lazy(() => import("./components/Cart"));
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
         <Header />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<div className="loading">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Router>
     </Provider>
   );
